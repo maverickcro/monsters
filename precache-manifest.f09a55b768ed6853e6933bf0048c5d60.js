1856f6001ef356fd58e3be7291a69e066c794e8f
maverickcro <mav3ricktv@gmail.com>
2020-08-31 12:40:16 +0200

    Updates

---
+++ precache-manifest.f09a55b768ed6853e6933bf0048c5d60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def52a81fd131fbfbaff87bd15852fd0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "56a3c8731f12695cfd48",
+    "url": "/monsters/static/css/main.ac7473a6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a3c8731f12695cfd48",
+    "url": "/monsters/static/js/main.a875e159.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
